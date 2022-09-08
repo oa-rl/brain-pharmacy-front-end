0d649b5e07c8e6231e702ec35679d333a94464b4
areyesoleon <areyes.oleon@gmail.com>
2022-09-07 23:18:02 -0600

    :construction:

--- src/app/branch/new-edit/new-edit.component.ts
+++ src/app/branch/new-edit/new-edit.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Notify } from 'notiflix';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CoreService } from 'src/app/core/core.service';
 import { FormComponent } from 'src/app/core/form.component';
+import { Api } from 'src/app/core/rest-api';
+import { BreadCrumbs } from 'src/app/models/main';
+import { Branch } from './../../models/company';
 
 @Component({
   selector: 'brain-new-edit',
@@ -11,17 +14,52 @@ import { FormComponent } from 'src/app/core/form.component';
 })
 export class NewEditComponent extends FormComponent implements OnInit {
 
-  constructor(public core:CoreService, protected builder: FormBuilder) { 
+  private _api: Api<Branch>;
+  private _id: number = 0;
+  public loading: boolean = false;
+  public breadCrum: Array<BreadCrumbs> = [
+    {
+      name: 'Lista',
+      path: '/branch/list'
+    },
+    {
+      name: 'Nueva sucursal',
+    }
+  ]
+
+  constructor(private _core: CoreService, protected builder: FormBuilder, private route: ActivatedRoute, private _route:Router) { 
     super();
+    this._api = this._core.resource('Branch');
   }
 
   ngOnInit(): void {
+    this._id = Number((this.route.snapshot.paramMap.get('id') || 0)?.toString())!;
     this.initForm();
+    if(this._id !== 0) {
+      this.find();
+    }
+  }
+
+  async find() {
+    this.loading = true;
+      this._form.disable();
+      this._core.savingOn();
+      try {
+        const data: Branch = await this._api.findById(this._id).toPromise();
+        this._form.patchValue(data);
+        this._form.enable();
+      } catch (error) {
+        
+      } finally {
+        this._core.savingOff();
+        this.loading = false;
+      }
   }
 
   private initForm() {
     this._form = this.builder.group({
-      id: null,
+      id: 0,
+      companyId: 1,
       name: [null, [Validators.required]],
       address: [null, [Validators.required]],
     });
@@ -34,14 +72,23 @@ export class NewEditComponent extends FormComponent implements OnInit {
     }
   }
 
-  save(value: boolean) {
-    if(value) {
-      console.log(this._form);
-      Notify.success('Guardado', {}, {backOverlayColor : '#4cda64'});
-    setTimeout(() => {
-      this.core.savingOff();
-    }, 3000);
+  async save(value: boolean) {
+    if (value && this.formIsValid()) {
+      try {
+        const opt = (this._id === 0) ? 'insert' : 'update';
+        await this._api[opt](this._form.value).toPromise();
+        this.goBack();
+        this.notifySuccess();
+      } catch (error) {
+        
+      } finally {
+        this._core.savingOff();
+      }
     }
+  }
+
+  goBack() {
+    this._route.navigate(['/branch/list']);
   }
 
 }
